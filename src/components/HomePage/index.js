509a79d6c13a191a314f00fd80a6a1a5ea804b2f
Ramyapriya-web <ramyapriya.posa@gmail.com>
2023-02-06 15:12:23 +0000

    project

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,45 @@
+import {FaArrowLeft} from 'react-icons/fa'
+import DetailsOfUser from '../DetailsOfUser'
+import './index.css'
+
+const HomePage = props => {
+  const {detList, loginList, location, timer} = props
+  const {imgUrl, qrUrl, code} = detList
+  const {nameInp} = loginList
+
+  return (
+    <div className="home-cont">
+      <FaArrowLeft
+        style={{
+          color: 'black',
+          fontSize: '20px',
+          alignSelf: 'flex-start',
+        }}
+      />
+      <img src={imgUrl} className="img" alt={nameInp} />
+      <div className="card-containers">
+        <p className="pp1"> {nameInp} </p>
+        <img src={qrUrl} className="img" alt={nameInp} />
+        <p className="pp2"> User Code </p>
+        <button type="button" className="code-but">
+          {code}
+        </button>
+      </div>
+      <div className="container-list">
+        <h1 className="heads"> Entry Log </h1>
+        <ul className="cont-lis">
+          {loginList.map(eachUser => (
+            <DetailsOfUser
+              info={eachUser.nameInp}
+              key={eachUser.id}
+              add={location}
+              timeDet={timer}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
